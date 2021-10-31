3bfcd30fba2b3070b34b87288babd65509177820
GaGiiiii <dragoslav.gagi8@gmail.com>
2021-10-31 12:15:40 +0100

    Started Docker

--- fat-cat-back/app/config/db.config.js
+++ fat-cat-back/app/config/db.config.js
@@ -1,8 +1,8 @@
 module.exports = {
-  HOST: "localhost",
-  USER: "root",
-  PASSWORD: "",
-  DB: "fatcatdb",
+  HOST: process.env.DB_HOST || "localhost",
+  USER: process.env.DB_USER || "root",
+  PASSWORD: process.env.DB_PASSWORD || "",
+  DB: process.env.DB_NAME || "fatcatdb",
   dialect: "mysql",
   pool: {
     max: 5,
